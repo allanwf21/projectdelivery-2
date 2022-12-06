80e458c24028289480f7227b8cf208dafbc7a9dd
Allan F <allanmatosf@hotmail.com>
2022-12-05 21:19:12 -0300

    Novas atualizações

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable react/jsx-no-undef */
 import './styles.css';
 import { ReactComponent as Logo } from './logo.svg';
 import { Link } from 'react-router-dom';
-
 
 function Navbar() {
   return (
