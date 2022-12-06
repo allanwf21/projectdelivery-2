0a7e21937ed60c3dd41c6a3edcc091800411edb5
Allan F <allanmatosf@hotmail.com>
2022-12-06 18:59:59 -0300

    Nova atualização 2.7

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -6,15 +6,15 @@ import { ReactComponent as IstagramIcon } from './instagram.svg';
 function Footer() {
   return (
     <footer className="main-footer">
-      App desenvolvido RedeCodeStyle
+      App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
       <div className="footer-icons">
-        <a href="https://www.youtube.com/" target="_new">
+        <a href="https://www.youtube.com/c/DevSuperior" target="_new">
           <YouTubeIcon />
         </a>
-        <a href="https://www.linkedin.com/" target="_new">
+        <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
           <LinkedinIcon />
         </a>
-        <a href="https://www.instagram.com/">
+        <a href="https://www.instagram.com/devsuperior.ig/">
           <IstagramIcon />
         </a>
       </div>
